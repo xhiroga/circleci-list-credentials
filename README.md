0c44f43324c93001dfa11e6bbb961d5530a25bbc
xhiroga <13391129+xhiroga@users.noreply.github.com>
2023-01-05 15:09:21 +0900

    feat: get project env vars

--- README.md
+++ README.md
@@ -3,29 +3,36 @@ Listing up CircleCI Credentials
 
 ## How to run
 
-
 ### 1. Get CircleCI API Token
-
-
 
 1. Go [Personal API Tokens](https://app.circleci.com/settings/user/tokens) and create new token.
 2. `cp .env.sample .env` and paste your API Token.
 
 ### 2. List up projects
 
+At first, `touch projects.txt`.
+
 Copying projects name like this.
 
 https://user-images.githubusercontent.com/13391129/210705946-31dfbaf8-6abf-4590-ae36-98a049fcad0c.mov
 
-After that, save `projects.txt`. (`Set Up Project` will be ignored)
+After that, save `projects.txt`. 
 
-### 3. Run script
+#### Note
+
+- `Set Up Project` will be ignored.
+- Only github.com projects are covered.
+
+### 3. Set project prefix
+
+Set `PROJECT_PREFIX` in `.env`.
+
+### 4. Run script
 
 ```shell
 npm i
 npm run app > logs/credentials.log
 ```
-
 
 ### (Optional) Deactivate CircleCI API Token
 
--- index.ts
+++ index.ts
@@ -1,3 +1,63 @@
-import 'dotenv/config'
+import "dotenv/config";
+import * as fs from "fs";
+import fetch from "node-fetch";
 
-console.log(process.env.API_TOKEN)
+const { API_TOKEN, PROJECT_PREFIX } = process.env;
+if (API_TOKEN == undefined || PROJECT_PREFIX == undefined) {
+  throw new Error("Environment variables are not configured.");
+}
+
+const readConnectedProjects = () => {
+  const text = fs.readFileSync("projects.txt", "utf8");
+  const linesFromEnd = text.split("\n").reverse();
+  const projects: string[] = [];
+  let i = 0;
+  while (i < linesFromEnd.length) {
+    const line = linesFromEnd[i];
+    if (["", "Follow Project", "Unfollow Project"].includes(line)) {
+      i++;
+      continue;
+    } else if (line === "Set Up Project") {
+      i = i + 2;
+    } else {
+      projects.push(line);
+      i++;
+    }
+  }
+  return projects.reverse();
+};
+
+const getEnvvarItems = async (
+  projectName: string,
+  pageToken?: string
+): Promise<string[]> => {
+  const headers = {
+    authorization: `Basic ${Buffer.from(API_TOKEN).toString("base64")}`,
+  };
+  const response = await fetch(
+    `https://circleci.com/api/v2/project/${PROJECT_PREFIX}/${projectName}/envvar${
+      pageToken ? `?page-token=${pageToken}` : ""
+    }`,
+    { headers }
+  );
+  const data = await response.json();
+  if (data.items != undefined) {
+    const next: string[] = data.next_page_token
+      ? await getEnvvarItems(projectName, data.nextPageToken)
+      : [];
+    return [...data.items, next];
+  } else if (data.message === "Project not found") {
+    return [];
+  } else {
+    throw new Error(data);
+  }
+};
+
+const app = async () => {
+  const connectedProjects = readConnectedProjects();
+  connectedProjects.forEach(async (projectName) => {
+    const items = await getEnvvarItems(projectName);
+    console.log(`{${projectName}, ${JSON.stringify(items)}}`);
+  });
+};
+app();
